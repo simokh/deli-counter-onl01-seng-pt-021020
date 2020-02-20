cd2d1432f7c9e79ee8dce4783a6f1a3f7343dbf8
Simo Khouili <m.simokhouili@gmail.com>
2020-02-20 23:43:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def line(array)
     deli.push(name)
   # use the push method for Append — Pushes the given object(s) on to the end of this array. This expression returns the array itself, so several appends may be chained together.
     puts "Welcome, #{name}. You are number #{deli.length} in line."
-  # return the person's name along with their position in line by using the hashtag, swingly brackets to call every arguement's iteration
+  
   end
 
   def now_serving(line)
