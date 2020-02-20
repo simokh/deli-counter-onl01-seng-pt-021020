315c5fd97b5c7473d8c6ee271c3374e3c64d0aad
Simo Khouili <m.simokhouili@gmail.com>
2020-02-20 23:44:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,9 +17,9 @@ def line(array)
 
   def take_a_number(deli, name)
     deli.push(name)
-  # use the push method for Append — Pushes the given object(s) on to the end of this array. This expression returns the array itself, so several appends may be chained together.
+  array itself, so several appends may be chained together.
     puts "Welcome, #{name}. You are number #{deli.length} in line."
-  
+
   end
 
   def now_serving(line)
